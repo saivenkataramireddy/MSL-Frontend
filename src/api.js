8c92461cc313d8722edb5a2b6ff14552be0a3e9c
saivenkataramireddy <saivenkataramireddykesara@gmail.com>
2026-02-20 12:14:23 +0530

    new backend link connected

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://msl-6i5l.onrender.com',
+  baseURL: 'https://msl-6i5l.onrender.com/',
 });
 
 // Attach token to every request
