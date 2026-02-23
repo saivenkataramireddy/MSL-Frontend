7789d8d9c67ed17c905fb911ecb7a30787e7b2c2
saivenkataramireddy <saivenkataramireddykesara@gmail.com>
2026-02-23 11:46:47 +0530

    backend api integrated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://msl-6i5l.onrender.com',
+  baseURL: 'https://msl-backend-6uej.onrender.com/',
 });
 
 api.interceptors.request.use((config) => {
